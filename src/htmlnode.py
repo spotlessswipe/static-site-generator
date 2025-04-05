87948fc8dc89bda7d786575abc3b7d6adda23545
spotlessswipe <spotlessswipe@pm.me>
2025-04-05 05:32:53 +0200

    html leaf, tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,20 @@ class HTMLNode:
 
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+            self,
+            tag: str,
+            value: str,
+            props: dict[str, str] = None
+    ):
+        super().__init__(tag, value, [], props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -48,6 +48,37 @@ class TestHTMLNode(unittest.TestCase):
         printed = str(node)
         self.assertEqual("HTMLNode(p, test, [], {'href': 'https://www.google.com', 'target': '_blank'})", printed)
 
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_link(self):
+        node = LeafNode(
+            "a",
+            'link',
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com" target="_blank">link</a>'
+            )
+
+    def test_leaf_no_value_raises_error(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", None)
+            node.to_html()
+
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", "")
+            node.to_html()
+
+    def test_leaf_no_tag(self):
+        node = LeafNode("", "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
