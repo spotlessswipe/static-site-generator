b9b4bb9f5a02a276411b4e0ab1e07e39848c46b6
spotlessswipe <spotlessswipe@pm.me>
2025-04-06 19:36:05 +0200

    page to git

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ import os
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +18,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -25,7 +27,7 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     entries = os.listdir(dir_path_content)
     print(entries)
 
@@ -38,9 +40,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             extension = os.path.splitext(entry)[1]
             if extension == '.md':
                 destination_path = destination_path[:-3] + '.html'
-                generate_page(full_path, template_path, destination_path)
+                generate_page(full_path, template_path, destination_path, basepath)
         elif os.path.isdir(full_path):
-            generate_pages_recursive(full_path, template_path, destination_path)
+            generate_pages_recursive(full_path, template_path, destination_path, basepath)
 
 
 
