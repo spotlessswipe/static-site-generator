22dff025cacf10c81d4cc0031b8c4316eb55b2ec
spotlessswipe <spotlessswipe@pm.me>
2025-04-06 17:35:26 +0200

    html2

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
-from htmlnode import HTMLNode
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    text_node = TextNode('This is some anchor text', TextType.BOLD, 'https://www.boot.dev')
-    print(text_node)
-    html_node = HTMLNode(
-        "p",
-        'test',
-        [],
-        {
-            "href": "https://www.google.com",
-            "target": "_blank",
-        }
-    )
-    print(html_node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-if __name__ == "__main__":
-    main()
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
